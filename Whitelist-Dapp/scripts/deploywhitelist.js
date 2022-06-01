a292a65c15ed444eff5df151a1eb1976efd22b53
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-01 18:43:37 +0000

    Config File

---
+++ Whitelist-Dapp/scripts/deploywhitelist.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+
+const main = async()=>{
+    const Whitelistcontract = await ethers.getContractFactory("Whitelist");
+    const deployedwhitelist = await Whitelistcontract.deploy(10);
+    await deployedwhitelist.deployed();
+
+    console.info(`Deployed Whitelist Contract Address:${deployedwhitelist.address}`)
+
+}
+
+main().then(()=>{
+   
+    console.info("succesful deployment");
+    process.exit(0).
+    catch((err)=>{
+        console.log("ha ocurrido un error !");
+        console.log(err);
+        process.exit(1);
+    })
+})
