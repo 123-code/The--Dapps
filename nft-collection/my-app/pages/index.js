e9db35c1fb7c498fd1c761456bc634ec7fee84f3
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-09-01 21:24:54 +0000

    Working on Next.js Frontend

--- nft-collection/my-app/pages/index.js
+++ nft-collection/my-app/pages/index.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {useRef,useState,useEffect} from 'react';
 import Web3Modal from 'web3modal';
-import { providers } from 'ethers';
+import { Contract, providers } from 'ethers';
+import {NFT_CONTRACT_ADDRESS,NFT_CONTRACT_ABI} from "../constants";
+// deployed 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 
 
@@ -11,11 +13,82 @@ import { providers } from 'ethers';
 
 export default function Home() {
   const [walletconnected,setwalletconnected] = useState(false);
+  const [presalestarted,setpresalestarted] = useState(false);
+  const [isowner,setisowner] = useState(false);
+  const [presaleended,setpresaleended] = useState(false);
   const web3modalref = useRef();
+
+// function gets us the owner of the contract.
+const getcontractowner = async()=>{
+  try{
+  const signer = await getproviderorsigner(true);
+
+  const nftcontract = new Contract(NFT_CONTRACT_ADDRESS,NFT_CONTRACT_ABI,signer);
+
+  const owner = nftcontract.owner();
+  if(owner.toLowerCase === signer ){
+    console.info("same")
+  }
+  else{
+    console.info("Error");
+  }
+
+
+  }
+  catch{(err)=>{
+console.error(err);
+  }}
+  
+}
+
+
+const startpresale = async ()=>{
+  try{
+    const signer = await getproviderorsigner(true);
+    const nftcontract = new Contract(NFT_CONTRACT_ADDRESS,NFT_CONTRACT_ABI,signer);
+    const tx = await nftcontract.startPresale();
+    await tx.wait();
+
+
+  }
+  catch{(err)=>{
+console.error(err);
+  }}
+
+}
+
+const checkpresaleended = async()=>{
+  try{
+    const provider = await getproviderorsigner();
+    const nftcontract = new Contract(NFT_CONTRACT_ADDRESS,NFT_CONTRACT_ABI,provider);
+   
+    
+  const presaleendtime = await nftcontract.presaleEnded();
+  //date in seconds.
+  const now = Date.now()/1000;
+const haspresaleended = presaleendtime.lt(Math.floor(now));
+setpresaleended(false);
+
+
+  }catch{(err)=>{
+    console.error(err);
+  }};
+}
 
 
 const checkpresalestarted = async ()=>{
-  const provider = await getproviderorsigner();
+  try{
+    const provider = await getproviderorsigner();
+    const nftcontract = new Contract(NFT_CONTRACT_ADDRESS,NFT_CONTRACT_ABI,provider);
+    const presalestarted = await nftcontract.presaleStarted();
+    setpresalestarted(presalestarted);
+
+
+
+  }catch{(err)=>{
+    console.error(err);
+  }};
+  
   
 
 }
@@ -60,6 +133,7 @@ return web3provider;
       )
 
     }
+    checkpresalestarted();
 
   },[])
   return (
