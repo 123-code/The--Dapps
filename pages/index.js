d541c398b5d872960d83251528fe146898c16425
123-code <naranjojose256@gmail.com>
2022-09-18 22:59:18 -0500

    Dapp Frontend

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Web3Modal from 'web3modal';
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../../Constants";
-
+ 
 export default function Home(){
   const [WalletConnected,setWalletConnected] = useState(false);
   const [numofWhitelisted,setNumOfWhitelisted] = useState(0);
