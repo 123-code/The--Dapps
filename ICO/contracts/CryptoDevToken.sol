cc0909e345e3448745f9c5ca4de6322048734eb6
123-code <naranjojose256@gmail.com>
2022-10-16 22:07:20 -0500

    Initial contract Commit

---
+++ ICO/contracts/CryptoDevToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^ 0.8.10;
+
+// ERC20 token Contract 
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptodevs.sol";
+
+contract CrtptoDevToken is ERC20,Ownable{
+
+}
--- ICO/contracts/ICryptodevs.sol
+++ ICO/contracts/ICryptodevs.sol
@@ -1,10 +1,9 @@
 pragma solidity ^ 0.8.9;
 
 interface ICryptodevs{
+    //the input is the address and the index, and the output is the tokenID
+    // check token ID hasn't already been used.
     function tokenOfOwnerByIndex(address owner,uint256 index)external view returns(uint256 tokenid);
+    // the input is the address of the NFT owner, the output id the balance in the NFT contract.
     function getebalanceof(address owner) external view returns(uint256 balance );
-    
-
-
-    
 }
