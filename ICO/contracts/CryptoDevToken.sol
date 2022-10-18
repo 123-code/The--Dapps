b7e8cdedd4c5dde9670331d7abcfbf2287fc3c4a
123-code <naranjojose256@gmail.com>
2022-10-18 12:32:37 -0500

    New contract LOL

--- ICO/contracts/CryptoDevToken.sol
+++ ICO/contracts/CryptoDevToken.sol
@@ -6,5 +6,50 @@ import "openzeppelin/contracts/access/Ownable.sol";
 import "./ICryptodevs.sol";
 
 contract CrtptoDevToken is ERC20,Ownable{
+uint256 claimable = 0;
+uint256 public constant tokenprice = 0.001 ether;
+uint256 public constant tokenspernft = 10*10**18;
+ICryptodevs cryptodevnft;
+mapping(uint256=>bool) public tokenidsclaimed;
+
+constructor(address _nftcontract) ERC20("CryptoDevToken","CT"){
+    // instance of the nftcontract here
+    cryptodevnft = ICryptodevs(_nftcontract);
+}
+
+// claims 10 tokens per owned NFT 
+function claim()public{
+    address sender = msg.sender;
+    uint256 balance = cryptodevnft.balanceOf(sender);
+    require(balance < 0,"You dont own any nfts");
+
+//claim tokens based on the balance
+    for(uint256 i=0;i<balance;i++){
+        uint amounttoclaim = 0;
+        // get tokenID of each NFT owned by msg.sender
+        uint256 tokenID = cryptodevnft.tokenOfOwnerByIndex(sender,i);
+        if(!tokenidsclaimed[tokenID]){
+            tokenidsclaimed[tokenID] = true;
+            amounttoclaim += 1;
+
+        }
+        
+    }
+require(amounttoclaim > 0,"You jave minted all of your tokens");
+// the mint function takes a big number as an argument. 
+_mint(msg.sender,amount*10);
+}
+
+// minting ERC-20
+function mint(uint256 amount) public payable {
+    uint256 _requiredamount = tokenprice * amount;
+    require(msg.value >= _requiredamount,"Error en la compra!");
+
+
+
+}
+
+
+
 
 }
