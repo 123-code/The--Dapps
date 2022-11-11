24e2b032bd65e94d2f19e9cf7f4862d4cb1a5298
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-11-11 22:32:04 +0000

    Added Files

--- ICO/my-app/pages/index.js
+++ ICO/my-app/pages/index.js
@@ -1,69 +1,82 @@
+import {React,useRef,useState,useEffect} from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState,useEffect,useRef } from 'react'
-import Web3Modal from 'web3modal'
+import Web3Modal from 'web3modal';
+import { providers,BigNumber } from "ethers";
 
 
-
+ 
 export default function Home() {
-  const [walletconnected,setwalletconnected] = useState(false);
-  // user wallet gets connnected when site is first opened 
-
-const getProviderOrSigner = ()=>{
-  try{
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider)
-    const {chainID} = web3Provider.getNetwork();
-    if(chainID !== 5){
-      window.alert("not connected to goerli")
-      throw new Error("not connected to goerli")
-    }
-
-  }catch(err){
-
-  }
-}
+  const zero = BigNumber.from(0);
+  const Web3ModalRef = useRef();
+  const [walletConnected,setwalletConnected] = useState(false);
+  const [tokensminted,settokensminted] = useState(zero);
 
 
-  const connectwallet = async()=>{
-    try{
+
+  const connectwallet = async () => {
+    try {
+
       await getProviderOrSigner();
-      setwalletconnected(true)
-
-    }catch(err){
-console.error(err)
+      setwalletConnected(true);
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
 
-  useEffect(()=>{
-    const web3ModalRef = useRef();
-    if(!walletconnected){
-      web3ModalRef.current = new Web3Modal({
-        network:'goerli',
-        providerOptions:{},
-        disableInjectedProvider:false
-      })
-connectWallet()
-    }
-
-  },[])
-
-  return (
+  const getProviderOrSigner = async (needSigner = false) => {
     
+    const provider = await Web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to goerli");
+      throw new Error("Change network to goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+    useEffect(()=>{
+      if(!walletConnected){
+        Web3ModalRef.current = new Web3Modal({
+          network:"goerli",
+          providerOptions:{},
+          disableInjectedProvider:false,
+        });
+        connectwallet();
+      }
+
+
+    },[])
+
+  
+
+
+  return(
    <div>
     <Head>
-    <title> Cryptodevs ICO </title>
-    <meta name="description" content="ICO-DAPP"/>
-      </Head>
-      <div className={style.main}>
+    
+    </Head>
+    <h1 >Welcome to Crypto Devs ICO!</h1>
+<div className={styles.main}>
+
 <div>
-  <h1 className={styles.title}> Welcome to CryptoDevs ICO </h1>
-  <div className = {styles.description}>
-    You can mint cryptoDevtokens here 
-  </div>
+{!walletConnected ? (<button onClick={connectwallet} className={styles.button}> connect wallet </button>)
+: <div> connected </div>}
 </div>
-      </div>
-    </div>
+
+
+ 
+
+</div>
+   </div>
+     
   )
 }
