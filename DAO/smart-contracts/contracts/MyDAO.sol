27fc0c9688c0b8002eab1fb0b0fff8a966d8ff53
123-code <naranjojose256@gmail.com>
2022-11-30 17:26:01 -0500

    DAO contract updates

--- DAO/smart-contracts/contracts/MyDAO.sol
+++ DAO/smart-contracts/contracts/MyDAO.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^ 0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -34,7 +35,7 @@ struct Proposal{
     mapping(uint256=>bool) nftused;
 }
 
-mapping(uint256=>Proposal) nfttoproposal;
+mapping(uint256=>Proposal) nftproposal;
 uint256 proposalcounter;
 
 ICryptodevs CryptodevsContract;
@@ -47,15 +48,35 @@ constructor(address _nftmarketplace,address _cryptodevcontract)payable{
     nftMarketplace = IFakeNFTMarketplace(_nftmarketplace); 
 }
 
+// modifiers.
 modifier NFTHolderOnly(){
     require(CryptodevsContract.balanceOf(msg.sender)>0,"NOT A DAO MEMBER!!");
     _;
 }
 
-
-function createproposal() external {
-
+modifier DeadlineNotExceeded(uint256 pindex){
+    require(block.timestamp < nftproposal[pindex].deadline,"DEADLINE EXCEEEDED"); _;
 }
+
+
+// gets the token id to buy  as input to create proposal;
+function createproposal(uint256 _token_id) external NFTHolderOnly returns(uint256){
+require(nftMarketplace.checkisavailable(_token_id),"NFT not available");
+// Proposal struct instance 
+Proposal storage proposal = nftproposal[proposalcounter];
+proposal._nft_id = _token_id;
+proposal.deadline = block.timestamp + 5 minutes;
+proposalcounter += 1;
+return proposalcounter-1;
+}
+
+// enum listing only two possible options for a vote
+enum voteoptions{
+    YES,
+    NO 
+}
+
+
 
 
 }
