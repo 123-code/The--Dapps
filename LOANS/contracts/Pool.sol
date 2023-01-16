0b421aceea0bdbd6148c1727297d07f4c4033717
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-01-16 20:11:40 +0000

    pool contract bugs fixed

--- LOANS/contracts/Pool.sol
+++ LOANS/contracts/Pool.sol
@@ -4,25 +4,37 @@ contract Pool{
 
 mapping(address=>uint) public stakedbalance;
 mapping(address => uint) public LPtokens;
+mapping(address => bool) public hasstaked;
+
 uint total;
 uint deposit;
+uint amount;
 
-function stake() public payable {
+
+function stake(uint256 _amount) public payable {
     //address (this).transfer(_amount);
-   
+    amount = _amount;
 }
 
-function DistributeLPs()public  returns(uint){
+/*There is no mechanism in place to prevent malicious users from calling the "DistributeLPs" 
+function multiple times and receiving more LP tokens than they are entitled to.*/
+function DistributeLPs()public returns(uint){
+    require(hasstaked[msg.sender] == true);
     for(uint i=0;i<LPtokens[msg.sender];i++){
 deposit += 1;
     }
+    LPtokens[msg.sender] = deposit;
     return deposit;
+    
 }
 
  receive() external payable {
+     //require(msg.sender)
+    require(msg.value > 0.001 ether);
      stakedbalance[msg.sender] += msg.value;
      LPtokens[msg.sender] += msg.value;
      total += msg.value;
+    payable(address(this)).transfer(amount);
  }
 
 
