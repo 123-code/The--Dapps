190a709244d91ae8ee61c8a0dda5318d49fbab21
123-code <naranjojose256@gmail.com>
2023-03-01 18:25:08 -0500

    Contract Updates

--- LOANS/contracts/Pool.sol
+++ LOANS/contracts/Pool.sol
@@ -5,36 +5,37 @@ contract Pool{
 mapping(address=>uint) public stakedbalance;
 mapping(address => uint) public LPtokens;
 mapping(address => bool) public hasstaked;
+mapping(address=>bool)public distributed;
 
 uint total;
 uint deposit;
 uint amount;
 
 
-function stake(uint256 _amount) public payable {
-    //address (this).transfer(_amount);
-    amount = _amount;
-}
 
 /*There is no mechanism in place to prevent malicious users from calling the "DistributeLPs" 
 function multiple times and receiving more LP tokens than they are entitled to.*/
-function DistributeLPs()public returns(uint){
+function DistributeLPs()public{
     require(hasstaked[msg.sender] == true);
+    require(distributed[msg.sender] == false,"already received LPs");
     for(uint i=0;i<LPtokens[msg.sender];i++){
 deposit += 1;
     }
     LPtokens[msg.sender] = deposit;
-    return deposit;
-    
+    distributed[msg.sender] = true;
+}
+
+function getLPsByAddress(address search)public view returns(uint){
+
 }
 
  receive() external payable {
      //require(msg.sender)
-    require(msg.value > 0.001 ether);
+    require(msg.value > 0.001 ether,".I.");
      stakedbalance[msg.sender] += msg.value;
      LPtokens[msg.sender] += msg.value;
      total += msg.value;
-    payable(address(this)).transfer(amount);
+    payable(address(this)).transfer(msg.value);
  }
 
 
