c6fd5744e8432cdc3607bbbf66494bd8553f793e
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-03-29 02:35:08 +0000

    infura url falta

--- LOANS/loans-frontend/pages/_app.js
+++ LOANS/loans-frontend/pages/_app.js
@@ -1,7 +1,53 @@
+import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi'
+ 
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+ 
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import Profile from './'
 import '../styles/globals.css'
 
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [infuraProvider({ apiKey: 'https://goerli.infura.io/v3/682c39bac1294baeb74ae767786db1ca' }), publicProvider()],
+)
+
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: '...',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <WagmiConfig client={client}>
+  <Profile />
+</WagmiConfig>
 }
 
 export default MyApp
